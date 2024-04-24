affec911b0e82c85387dd169a79e7514e7c54d30
JinnLynn <eatfishlin@gmail.com>
2024-04-24 12:52:33 +0800

    fix: enqueue new trackers

--- newTrackon/trackerlist_project.py
+++ newTrackon/trackerlist_project.py
@@ -1,18 +1,32 @@
 from time import sleep
+import logging
 
 import requests
 
 from newTrackon import trackon
 
+logger = logging.getLogger("newtrackon")
+
 tracker_list = [
     "https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all.txt",
-    "https://raw.githubusercontent.com/XIU2/TrackersListCollection/master/all.txt"
+    "https://raw.githubusercontent.com/XIU2/TrackersListCollection/master/best.txt"
 ]
 
 
 def main():
     while True:
-        for tl in tracker_list:
-            tlist = requests.get(tl)
-            trackon.enqueue_new_trackers(tlist.text)
+        trackers = ''
+        for url in tracker_list:
+            logger.info(f"Download new trackers from: {url}")
+            try:
+                tlist = requests.get(url)
+                if tlist.status_code == 200:
+                    trackers += tlist.text
+                else:
+                    logger.warn(f"Download fail: {tlist.status_code}")
+            except Exception as e:
+                logger.warn(f"Download fail: {e}")
+        tracker_count = len(trackers.split())
+        logger.info(f"Enqueue {tracker_count} new trackers")
+        trackon.enqueue_new_trackers(trackers)
         sleep(86400)
