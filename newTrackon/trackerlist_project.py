36900916cadd04fb2ec8d9ea1c793b13afccf48d
JinnLynn <eatfishlin@gmail.com>
2024-04-24 10:21:06 +0800

    feat: more tracker list

--- newTrackon/trackerlist_project.py
+++ newTrackon/trackerlist_project.py
@@ -4,11 +4,15 @@ import requests
 
 from newTrackon import trackon
 
+tracker_list = [
+    "https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all.txt",
+    "https://raw.githubusercontent.com/XIU2/TrackersListCollection/master/all.txt"
+]
+
 
 def main():
     while True:
-        tlist = requests.get(
-            "https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all.txt"
-        )
-        trackon.enqueue_new_trackers(tlist.text)
+        for tl in tracker_list:
+            tlist = requests.get(tl)
+            trackon.enqueue_new_trackers(tlist.text)
         sleep(86400)
